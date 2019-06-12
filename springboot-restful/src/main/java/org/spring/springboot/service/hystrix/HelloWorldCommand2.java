7a87a59aba630497da16e44ac841241ced144269
yuanmenghaixin <710584845@qq.com>
2019-06-12 09:15:04 +0800

    hystrix学习

---
+++ springboot-restful/src/main/java/org/spring/springboot/service/hystrix/HelloWorldCommand2.java
@@ -0,0 +1,39 @@
+package org.spring.springboot.service.hystrix;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Tim on 2019/6/11.
+ */
+public class HelloWorldCommand2 extends HystrixCommand<String> {
+    private final String name;
+
+    public HelloWorldCommand2(String name) {
+        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("HelloWorldGroup"))
+                /* 配置依赖超时时间,500毫秒*/.andCommandPropertiesDefaults(HystrixCommandProperties.Setter().withExecutionIsolationThreadTimeoutInMilliseconds(500)));
+        this.name = name;
+    }
+
+    @Override
+    protected String getFallback() {
+        return "exeucute Falled";
+    }
+
+    @Override
+    protected String run() throws Exception {
+        //sleep 1 秒,调用会超时
+        TimeUnit.MILLISECONDS.sleep(1000);
+        return "Hello " + name + " thread:" + Thread.currentThread().getName();
+    }
+
+    public static void main(String[] args) throws Exception {
+        HelloWorldCommand command = new HelloWorldCommand("test-Fallback");
+        String result = command.execute();
+        System.out.println("结果："+result);
+    }
+
+}
---
+++ springboot-restful/src/main/java/org/spring/springboot/service/hystrix/HystrixService.java
@@ -0,0 +1,8 @@
+package org.spring.springboot.service.hystrix;
+
+/**
+ * Created by Tim on 2019/6/11.
+ */
+public class HystrixService {
+
+}
